98cdcb8252df5a82d71c1f202c4097ba929c1609
binghujuelie <binghujuelie@foxmail.com>
2022-03-22 20:40:00 +0800

    Site updated: 2022-03-22 20:39:59

---
+++ dist/music.js
@@ -0,0 +1,61 @@
+const ap = new APlayer({
+  container: document.getElementById('aplayer'),
+  fixed: true,
+  autoplay: false,
+    audio: [
+	  {
+      name: "Fugue In G Minor BWV 578",
+      artist: 'Helmut Walcha',
+      url: 'http://music.163.com/song/media/outer/url?id=35957808.mp3',
+      cover: 'http://p2.music.126.net/wyXmIzmcAMDi-vD9UsAnlA==/3285340745228831.jpg?param=130y130',
+    },
+    {
+      name: "Toccata and Fugue in D minor, BWV565:Toccata",
+      artist: 'Helmut Walcha',
+      url: 'http://music.163.com/song/media/outer/url?id=35957799.mp3',
+      cover: 'http://p2.music.126.net/wyXmIzmcAMDi-vD9UsAnlA==/3285340745228831.jpg?param=130y130',
+    },
+    {
+      name: "Toccata and Fugue in D minor, BWV565:Fugue",
+      artist: 'Helmut Walcha',
+      url: 'http://music.163.com/song/media/outer/url?id=35957800.mp3',
+      cover: 'http://p2.music.126.net/wyXmIzmcAMDi-vD9UsAnlA==/3285340745228831.jpg?param=130y130',
+    },
+    {
+      name: "Toccata, Adagio and Fugue in C major, BWV564:Adagio",
+      artist: 'Helmut Walcha',
+      url: 'http://music.163.com/song/media/outer/url?id=35957802.mp3',
+      cover: 'http://p2.music.126.net/wyXmIzmcAMDi-vD9UsAnlA==/3285340745228831.jpg?param=130y130',
+    },
+    {
+      name: "Fantasy and Fugue in G minor, BWV542:Fantasy",
+      artist: 'Helmut Walcha',
+      url: 'http://music.163.com/song/media/outer/url?id=35957804.mp3',
+      cover: 'http://p2.music.126.net/wyXmIzmcAMDi-vD9UsAnlA==/3285340745228831.jpg?param=130y130',
+    },
+    {
+      name: "Passacaglia and Fugue in C minor, BWV582:Passacaglia",
+      artist: 'Helmut Walcha',
+      url: 'http://music.163.com/song/media/outer/url?id=35957806.mp3',
+      cover: 'http://p2.music.126.net/wyXmIzmcAMDi-vD9UsAnlA==/3285340745228831.jpg?param=130y130',
+    },
+    {
+      name: "Passacaglia and Fugue in C minor, BWV582:Fugue",
+      artist: 'Helmut Walcha',
+      url: 'http://music.163.com/song/media/outer/url?id=35957807.mp3',
+      cover: 'http://p2.music.126.net/wyXmIzmcAMDi-vD9UsAnlA==/3285340745228831.jpg?param=130y130',
+    },
+    {
+      name: "Ich ruf zu Dir, Herr Jesu Christ, BWV639",
+      artist: 'Helmut Walcha',
+      url: 'http://music.163.com/song/media/outer/url?id=35957809.mp3',
+      cover: 'http://p2.music.126.net/wyXmIzmcAMDi-vD9UsAnlA==/3285340745228831.jpg?param=130y130',
+    },
+    {
+      name: "Nun komm, der Heiden Heiland, BWV659",
+      artist: 'Helmut Walcha',
+      url: 'http://music.163.com/song/media/outer/url?id=35957810.mp3',
+      cover: 'http://p2.music.126.net/wyXmIzmcAMDi-vD9UsAnlA==/3285340745228831.jpg?param=130y130',
+    }
+    ]
+});
